27da20494e397ea597156bcdc5c285d8b7038b64
eye-yawn <140461256+eye-yawn@users.noreply.github.com>
2023-11-21 10:59:58 -0800

    Add files via upload

---
+++ pages/4_📜Visual_Timeline_of_an_Event_in_History.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+#streamlit side
+st.set_page_config(page_title="Timeline", page_icon="📜")
+
+st.markdown("# Visual Timeline of an Event in History")
+st.write(
+    """Coming soon!"""
+)
