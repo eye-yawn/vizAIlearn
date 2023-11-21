f7a41936677d1fc208483a4a0f914c121752b87d
eye-yawn <140461256+eye-yawn@users.noreply.github.com>
2023-11-21 01:45:07 -0800

    delete outdated 4_🕰Visual_Timeline_of_an_Event_in_History.py

--- 4_🕰Visual_Timeline_of_an_Event_in_History.py
+++ 4_🕰Visual_Timeline_of_an_Event_in_History.py
@@ -1,10 +1,1 @@
-import streamlit as st
 
-#streamlit side
-st.set_page_config(page_title="Timeline", page_icon="🕰")
-
-st.markdown("# Visual Timeline of an Event in History")
-st. title("Visual Timeline of an Event in History")
-st.write(
-    """Coming soon!"""
-)
