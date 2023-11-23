5e58955c35382de13cab3e60da8d935adb44305e
eye-yawn <140461256+eye-yawn@users.noreply.github.com>
2023-11-23 14:27:33 -0800

    Update 🏠Home.py

--- 🏠Home.py
+++ 🏠Home.py
@@ -1,17 +1,12 @@
 import streamlit as st
-import os
-import requests
-from io import BytesIO
-import fitz
-from bs4 import BeautifulSoup
-
 from PIL import Image
 
 st.set_page_config(page_title="VizAIlearn", page_icon="🎓")
 
 st.title("VizAIlearn - The Visual Learning Platform of the Future")
 st.write("Tired of sifting through endless pages of text? Worried about an upcoming test on several confusing concepts? \n\n This AI-powered tool is here to make your life easier. It can convert long-winded textbook pages into concise summaries while adding a variety of digestible visualizations to convey key ideas. \n\nOnce you're feeling comfortable, try the Q&A feature to instantly generate applicable practice questions and master knowledge. \nSay goodbye to stressful studying and hello to smarter, faster learning of the future with Viz-AI-learn")
-st.write('\nTo start using this app, click on one of the pages to the left. \nHappy Learning!\n\n')
+st.write('\nTo start using this app, click on one of the pages to the left. \nHappy Learning!\n')
+st.caption('Developed by Aayaan Ahmed -- GitHub: https://github.com/eye-yawn/vizAIlearn/tree/main')
 
 image = Image.open('stressed_student (1).jpg')
 
